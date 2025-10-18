0fd36ec783cbd539645ee1325bdfde7e17237907
Hallam Smith <me@hcssmith.com>
2025-10-18 18:18:28 +0100

    Move to buffer local variables, no autocmd

--- ftplugin/sh.lua
+++ ftplugin/sh.lua
@@ -1,12 +1,2 @@
-vim.cmd.compiler("make")
-
-local sh_cmds = vim.api.nvim_create_augroup('ShellAutocommands', { clear = true })
-
--- Create an autocmd that triggers when the filetype is 'sh'
-vim.api.nvim_create_autocmd('FileType', {
-  group = sh_cmds,
-  pattern = 'sh',
-  command = 'compiler make', -- This uses the built-in sh compiler
-})
-
-vim.print("running sh.lua")
+vim.print("Running sh.lua")
+vim.bo.makeprg = "make"
