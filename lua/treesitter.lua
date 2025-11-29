c923523e4e5222520467747139995240dc0d6167
hcssmith <me@hcssmith.com>
2025-11-29 21:33:03 +0000

    1 add treesitter configs (#18)
    * Add treesitter autostart rule & install script. Add any new parsers to the shell script and add alias in lua/treesitter.lua file if required

---
+++ lua/treesitter.lua
@@ -0,0 +1,25 @@
+local parsers = {
+  cs = "c_sharp"
+}
+
+
+vim.api.nvim_create_autocmd('FileType', {
+  callback = function(args)
+    local buf = args.buf
+    local ft = vim.bo[buf].filetype
+    local parser_name = parsers[ft] or ft
+
+    local ok = pcall(function()
+      vim.treesitter.start(buf, parser_name)
+    end)
+
+    if not ok then
+      vim.schedule(function()
+        vim.notify(
+          "TS: No parser for " .. parser_name,
+          vim.log.levels.WARN
+        )
+      end)
+    end
+  end
+})
