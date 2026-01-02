1e93702d6795fb3e9f8f33b558da9aa169ec6c30
Hallam Smith <me@hcssmith.com>
2026-01-02 00:14:13 +0000

    Fix command & root_marker

--- lsp/csharp_ls.lua
+++ lsp/csharp_ls.lua
@@ -1,14 +1,26 @@
 return {
-  cmd = { "csharp-ls" },
+  cmd = { vim.fn.expand("$HOME/.dotnet/tools/csharp-ls") },
   filetypes = { "cs" },
 
-  root_dir = function(fname)
-    local dir = vim.fs.dirname(fname)
+  root_dir = function(bufnr, on_dir)
+    local bufname = vim.api.nvim_buf_get_name(bufnr)
+    if bufname == "" then
+      on_dir(nil)
+      return
+    end
 
-    return vim.fs.find(
+    local start = vim.fs.dirname(bufname)
+
+    local match = vim.fs.find(
       { "*.sln", "*.csproj", ".git" },
-      { upward = true, path = dir }
+      { upward = true, path = start }
     )[1]
+
+    if match then
+      on_dir(vim.fs.dirname(match))
+    else
+      on_dir(nil)
+    end
   end,
   settings = {
     csharp = {
